fa9072344bbffc53d8759293b669578f6a0f6742
nikhil dagar <nikhildagar0361@gmail.com>
2024-11-29 15:53:48 +0530

    first

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# signLanguageDetection > 2024-11-29 9:32am
+https://universe.roboflow.com/object-detection-gq1bt/signlanguagedetection-jsb85
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ test.py
@@ -0,0 +1,17 @@
+# # import the inference-sdk
+# import inference_sdk
+# from inference_sdk import InferenceHTTPClient
+
+# # initialize the client
+# CLIENT = InferenceHTTPClient(
+#     api_url="https://detect.roboflow.com",
+#     api_key="8IMh8dZwwuwdCDPqfbTL"
+# )
+# from inference  import get_model
+# # infer on a local image
+# result = CLIENT.infer("test.jpg", model_id="signlanguagedetection-jsb85/1")
+
+# from inference import get_model
+# model_id='signlanguagedetection-jsb85/1'
+# Roboflow_Api_Key = userdata.get('8IMh8dZwwuwdCDPqfbTL')
+# model = get_model(model_id)
